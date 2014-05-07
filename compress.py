242e08d2a718ae039bd469f76c9b20bb7ff11cb1
Will Rogers <will.rogers@diamond.ac.uk>
2014-05-07 11:26:53 +0100

    Tidy compress.py slightly:
     - make output file exact size
     - accept file as argument

--- compress.py
+++ compress.py
@@ -1,6 +1,4 @@
-
-SYMBOL_FILE = 'mks937bHcg-symbol.edl'
-NEW_SYMBOL_FILE = 'mks937bHcg-symbol2.edl'
+import sys
 
 GROUP_HEADER = 'object activeGroupClass'
 GROUP_START = 'beginGroup'
@@ -9,7 +7,7 @@ GROUP_END = 'endGroup'
 groups = []
 current_group = []
 
-def detect_group(group):
+def locate_group(group):
     for line in group:
         if line.startswith('x'):
             x = int(line.split()[1])
@@ -47,11 +45,9 @@ def move_group(group, x_move, y_move):
                 in_x = False
                 new_group.append(line)
             else:
-                print "xline:", line
                 val = line.split()[1]
                 new_val = int(val) + x_move
                 new_group.append(line.replace(val, str(new_val)))
-                print "new xline:", line.replace(val, str(new_val))
         elif line.startswith('yPoints'):
             in_y = True
             new_group.append(line)
@@ -67,66 +63,91 @@ def move_group(group, x_move, y_move):
             new_group.append(line)
     return new_group
 
-with open(SYMBOL_FILE) as f:
+def new_name(filename):
+    '''
+    Just append a 2.
+    '''
+    parts = filename.split('.')
+    return '.'.join(parts[:-1]) + '2' + '.' + parts[-1]
 
-    in_group = False
+if __name__ == '__main__':
+    if not len(sys.argv) == 2:
+        print "Usage: %s <symbol-file>" % sys.argv[0]
 
-    for line in f:
-        if line.strip() == GROUP_HEADER:
-            in_group = True
-        elif line.strip() == GROUP_END:
-            in_group = False
-            groups.append(current_group)
-            current_group = []
+    filename = sys.argv[1]
+    new_filename = new_name(filename)
+    with open(filename) as f:
 
-        if in_group:
-            current_group.append(line)
+        in_group = False
+
+        for line in f:
+            if line.strip() == GROUP_HEADER:
+                in_group = True
+            elif line.strip() == GROUP_END:
+                in_group = False
+                groups.append(current_group)
+                current_group = []
+
+            if in_group:
+                current_group.append(line)
 
 
 
-print len(groups)
-x, y, width, height = detect_group(groups[0])
+    print 'Found %s symbols.' % len(groups)
+    x, y, width, height = locate_group(groups[0])
 
-start_x = 0
+    total_height = height
+    total_width = width * len(groups)
 
-moved_groups = []
+    start_x = 0
 
-for group in groups:
-    x, y, h, w = detect_group(group)
-    assert w == width
-    assert h == height
-    moved_groups.append(move_group(group, start_x - x, 0 - y))
-    start_x += width
+    moved_groups = []
 
-print len(moved_groups)
-for line in moved_groups[0]:
-    print line
+    for group in groups:
+        x, y, h, w = locate_group(group)
+        assert w == width
+        assert h == height
+        moved_groups.append(move_group(group, start_x - x, 0 - y))
+        start_x += width
 
-i = 0
-new_file = []
 
-with open(SYMBOL_FILE) as f:
-
-    in_group = False
+    # Create new file by passing through every line
+    i = 0
     new_file = []
+    changed_height = False
+    changed_width = False
 
-    for line in f:
-        if line.strip() == GROUP_HEADER:
-            in_group = True
-            new_file.extend(moved_groups[i])
-            i += 1
-        elif line.strip() == GROUP_END:
-            in_group = False
-            groups.append(current_group)
-            current_group = []
+    with open(filename) as f:
+        in_group = False
+        new_file = []
 
-        if not in_group:
-            new_file.append(line)
+        for line in f:
+            if not changed_height and line.split()[0] == 'h':
+                new_file.append('h %s\n' % total_height)
+                changed_height = True
+                continue
+            if not changed_width and line.split()[0] == 'w':
+                new_file.append('w %s\n' % total_width)
+                changed_width = True
+                continue
+            # if in a group, replace it with the moved group
+            if line.strip() == GROUP_HEADER:
+                in_group = True
+                new_file.extend(moved_groups[i])
+                i += 1
+            elif line.strip() == GROUP_END:
+                in_group = False
 
-print len(new_file)
+            if not in_group:
+                new_file.append(line)
 
-with open(NEW_SYMBOL_FILE, 'w') as f:
-    for line in new_file:
-        f.write(line)
+    print 'New file length: %s' % len(new_file)
+
+    # Write out the new file
+    with open(new_filename, 'w') as f:
+        for line in new_file:
+            f.write(line)
+
+    print 'Wrote new EDM symbol to %s' % new_filename
 
 
