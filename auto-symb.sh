ce483d70901a46c317c6ecd40d4851fe79f8ee26
Will Rogers <will.rogers@diamond.ac.uk>
2014-05-12 11:18:42 +0100

    Edit auto-symb.sh to convert one file at a time.
    Make the output easier to use with the converter program.

--- auto-symb.sh
+++ auto-symb.sh
@@ -1,37 +1,37 @@
-# Get everything on the path
 
-PATH=$PATH:/dls_sw/prod/R3.14.11/support/digitelMpc/4-27/data:/dls_sw/prod/R3.14.11/support/HostLink/3-0/data:/dls_sw/prod/R3.14.11/support/insertionDevice/4-13/data:/dls_sw/prod/R3.14.11/support/mks937a/2-69/data:/dls_sw/prod/R3.14.11/support/mks937b/2-8/data:/dls_sw/prod/R3.14.11/support/rackFan/2-4-1/data:/dls_sw/prod/R3.14.11/support/rga/4-4/data:/dls_sw/prod/R3.14.11/support/vacuum/3-38/data:/dls_sw/prod/R3.14.11/support/vacuumSpace/3-22/data:/dls_sw/prod/R3.14.11/support/dlsPLC/1-8/data:/dls_sw/prod/R3.14.11/support/vxStats/1-14/data:/dls_sw/prod/R3.14.11/support/devIocStats/3-1-5dls4/data
-
-# List of files we want to convert
-names="digitelMpcIonp-symbol digitelMpcTsp-symbol mks937aImg-symbol mks937aPirg-symbol rga-symbol vacuumValve-symbol"
+symbol_file=$1
+filename=`basename $symbol_file`
 symdir=symbols
 
-mkdir $symdir
 
-for name in $names; do
-    # Copy edl file
-    cp -f $(which $name.edl) $symdir/
+# Copy edl file
+cp -f $symbol_file $symdir/
 
-    # Check if it's old style EDM
-    if ! grep -q bgColor $symdir/$name.edl; then
-        edm -convert $symdir/$name.edl $symdir/$name.edl
-    fi
+# Check if it's old style EDM
+if ! grep -q bgColor $symdir/$filename; then
+    edm -convert $symdir/$filename $symdir/$filename
+fi
 
-    python compress.py $symdir/$name.edl
+echo $symdir/$filename
+python compress.py $symdir/$filename
 
-    # Replace background color with bright pink
-    sed -i -r '1,23 s/bgColor\s+index\s+[0-9]+/bgColor index 45/' $symdir/${name}2.edl
+# compress.py appends pixel size to name (eg x-33.edl)
+nfilename=$(basename $(ls $symdir/${filename%.*}-*.edl))
+nbase=${nfilename%.*}
+echo $nbase
 
-    # Wait for file to open with EDM, screenshot, then kill it
-    edm -x $symdir/${name}2.edl &
-    pid=$!
-    sleep 1
-    window_handle=$(xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)")
-    import -window ${window_handle:40} $symdir/$name.png
-    kill -9 $(pgrep -P $pid); kill -9 $pid
+# Replace background color with bright pink
+sed -i -r '1,23 s/bgColor\s+index\s+[0-9]+/bgColor index 45/' $symdir/$nfilename
 
-    # Replace bright pink with transparent and crop empty border
-    convert $symdir/$name.png -transparent \#ff00ff -trim +repage $symdir/$name.png
+# Wait for file to open with EDM, screenshot, then kill it
+edm -x $symdir/$nfilename &
+pid=$!
+sleep 1
+window_handle=$(xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)")
+import -window ${window_handle:40} $symdir/$nbase.png
+kill -9 $(pgrep -P $pid); kill -9 $pid
 
-done
+# Replace bright pink with transparent and crop empty border
+convert $symdir/$nbase.png -transparent \#ff00ff -trim +repage $symdir/$nbase.png
+
 
