a3365437ebe8bbd7361f35edb9d01384904177fe
Will Rogers <will.rogers@diamond.ac.uk>
2014-08-01 15:13:46 +0100

    Complete rewrite of layers.py
    It now creates a new clickable item for each one it finds.  The new
    clickable item is put at the same location but on top of all other
    widgets.

--- layers.py
+++ layers.py
@@ -3,28 +3,17 @@
 Ordering of elements in BOY screens are determined only by their 
 position in the xml file.
 
-Use elementtree to move:
- - invisible related displays
- - grouping container containing only invisible related displays
-to the bottom of an opi file, meaning that they are rendered
-at the top.
+Create an invisible Rectangle on top of any clickable 
+area which has the same click action.  Keep track of x and y
+position of any parent grouping containers, so that the 
+new Rectangle ends up in the correct place.
 
 Run this script after transforming all the relevant areas.
 '''
 
 import xml.etree.ElementTree as et
 import sys
-
-try:
-    path_file = sys.argv[1]
-except IndexError:
-    print "Usage: ", sys.argv[0], "<path-file>"
-    sys.exit()
-
-with open(path_file) as f:
-    lines = f.readlines()
-    lines = [line.strip() for line in lines if not line.startswith('#')]
-    lines = [line.strip() for line in lines if not line == '']
+import copy
 
 
 # BOY type IDs
@@ -38,88 +27,108 @@ REL_NAME = 'EDM related display'
 SHELL_NAME = 'EDM shell command'
 
 
-# invisible components are all rectangles, since buttons
-# cannot be transparent
-def invisible_related(node):
-    if not node.tag == 'widget':
-        return False
-    if not node.attrib['typeId'] == RECTANGLE:
-        return False
-    trans = True
-    rel = False
+def clickable_widget(node):
     for child in node:
-        if child.tag == 'transparent':
-            trans = child.text == 'true'
-        if child.tag == 'name':
-            rel = (child.text == REL_NAME or child.text == SHELL_NAME)
-    return rel and trans
+        if child.tag == 'actions':
+            if node.attrib['typeId'] in [ACTION_BUTTON, BOOL_BUTTON]:
+                return True
+            else:
+                try:
+                    return child.attrib['hook'] == 'true'
+                except:
+                    return True
+    return False
 
 
-def for_raising(node):
+def find_clickables(node, x, y):
     '''
-    Determine if any element is to be raised to the top.
+    Find all nodes which accept a mouse click
     '''
-    assert node.tag == 'widget'
-
-    type = node.attrib['typeId']
-    if type == GROUP:
-        print "fr for group"
-        for child in node:
-            if child.tag == 'widget':
-                if not invisible_related(child):
-                    return False
-        return True
-    elif type == RECTANGLE:
-        return invisible_related(node)
-    else:
-        return False
-
-def find_groups(node):
-    '''
-    remove any rule from linking containers which are any children of the node
-    '''
-    removed = []
+    if node.tag == 'widget' and node.attrib['typeId'] == GROUP:
+        x += int(node.find('x').text)
+        y += int(node.find('y').text)
+    clickables = []
     if len(node) == 0:
         return []
     else:
         for child in node:
             if child.tag == 'widget':
-                if for_raising(child):
-                    print "removing", child.attrib['typeId']
-                    node.remove(child)
-                    removed.append(child)
+                if clickable_widget(child):
+                    clickables.append((child, x, y))
                 else:
-                    removed.extend(find_groups(child))
+                    clickables.extend(find_clickables(child, x, y))
             else:
-                removed.extend(find_groups(child))
+                clickables.extend(find_clickables(child, x, y))
 
-    return removed
+    return clickables
 
 
-def fix_file(path):
+def create_new_clicker(node, x, y):
+    '''
+    Create new transparent rectangle.  Fetch the appropriate children of 
+    the node and copy them to the new widget.
+    Change position to that relative to any grouping containers.
+    '''
+    rect = et.Element('widget')
+    rect.attrib['typeId'] = RECTANGLE
+    v = et.SubElement(rect, 'transparent')
+    v.text = 'true'
+
+    atag = node.find('actions')
+    new_atag = copy.deepcopy(atag)
+    new_atag.attrib['hook'] = 'true'
+    rect.append(new_atag)
+
+    new_xtag = et.SubElement(rect, 'x')
+    xtag = node.find('x')
+    new_xtag.text = str(int(xtag.text) + x)
+    rect.append(new_xtag)
+
+    new_ytag = et.SubElement(rect, 'y')
+    ytag = node.find('y')
+    new_ytag.text = str(int(ytag.text) + y)
+    rect.append(new_ytag)
+
+    widthtag = node.find('width')
+    rect.append(widthtag)
+    heighttag = node.find('height')
+    rect.append(heighttag)
+
+    return rect
+
+def parse(path):
     tree = et.parse(path)
     root = tree.getroot()
 
-    clickables = find_groups(root)
+    clickables = find_clickables(root, 0, 0)
     print "There are %s clickables." % len(clickables)
-    # simply put all the invisible clickable elements at the bottom
-    # of the XML, where they are rendered last
-    root.extend(clickables)
 
-    print "\n\nthe clickables are:"
-
+    newcs = []
     for clicker in clickables:
-        for child in clicker:
-            if child.tag == 'name':
-                print child.text
+        newcs.append(create_new_clicker(*clicker))
 
+    root.extend(newcs)
 
     # write the new tree out to the same file
-    tree.write(path, encoding='utf-8', xml_declaration=True)
+    tree.write(file, encoding='utf-8', xml_declaration=True)
 
 
-for file in lines:
-    print "starting file ", file
-    fix_file(file)
+if __name__ == '__main__':
+
+    try:
+        path_file = sys.argv[1]
+    except IndexError:
+        print "Usage: ", sys.argv[0], "<path-file>"
+        sys.exit()
+
+    with open(path_file) as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines if not line.startswith('#')]
+        lines = [line.strip() for line in lines if not line == '']
+
+
+    for file in lines:
+        print "Parsing file ", file
+        parse(file)
 
 
