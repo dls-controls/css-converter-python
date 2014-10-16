dls-controls/css-converter-python

commit 40aa7129e1bc3e4719e9729425a9bf702062c687

Account for border width in symbol conversion
This assumes the border is 1px. Borders were being removed in the png
conversion process so the 'trim' option is no longer used for this.

compress.py
@@ -44,6 +44,7 @@ def locate_group(group):
             break
 
     return x, y, w, h
+    return x, y, w+1, h+1  # Account for border width
 
 def move_group(group, x_move, y_move):
     new_group = []

auto-symb.sh
@@ -32,7 +32,7 @@ import -window ${window_handle:40} $symdir/$nbase.png
 kill -9 $(pgrep -P $pid); kill -9 $pid
 
 # Replace bright pink with transparent and crop empty border
-convert $symdir/$nbase.png -transparent \#ff00ff -trim +repage $symdir/$nbase.png
+convert $symdir/$nbase.png -transparent \#ff00ff $symdir/$nbase.png
 
 
 echo $symdir/$nbase.png

commit 0c6846d9a59bb2a1a9ec00f0a8e57f87d5719d81

Fixed bug introduced in 40aa7129
Removing multiple return statments

compress.py
@@ -42,8 +42,6 @@ def locate_group(group):
         if line.startswith('y'):
             y = int(line.split()[1])
             break
-
-    return x, y, w, h
     return x, y, w+1, h+1  # Account for border width
 
 def move_group(group, x_move, y_move):