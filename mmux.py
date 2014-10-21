73ddaf3443c83533d29c9da7896b1319c962044b
Will Rogers <wahrogers@gmail.com>
2014-10-21 09:56:23 +0100

    Remove duplicate function, and pylint corrections.

--- mmux.py
+++ mmux.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env dls-python
 '''
-Menu mux widgets in EDM can't be automatically translated by the 
+Menu mux widgets in EDM can't be automatically translated by the
 converter, because they rely on dynamic macros which do not exist
 in CSS.
 
@@ -20,7 +20,7 @@ import sys
 
 from convert import make_writeable, make_read_only
 
-MENU_MUX_ID = "org.csstudio.opibuilder.widgets.edm.muxmenu"
+MENU_MUX_ID = 'org.csstudio.opibuilder.widgets.edm.muxmenu'
 
 
 def find_mm_symbols(node):
@@ -47,30 +47,6 @@ def find_mm_symbols(node):
 
     return symbols
 
-
-def find_mm_symbols(node):
-    '''
-    Recursively find all MenuMux symbols from root node.
-    '''
-    # If this widget is a grouping container, any child
-    # widgets need to specify coordinates plus coordinates
-    # of the grouping container.
-    symbols = []
-    if len(node) == 0:
-        return []
-    else:
-        for child in node:
-            if child.tag == 'widget':
-                if child.attrib['typeId'] == MENU_MUX_ID:
-                    t = child.find('targets')
-                    syms = t.findall('s')
-                    symbols.extend(set(sym.text for sym in syms))
-                else:
-                    symbols.extend(find_mm_symbols(child))
-            else:
-                symbols.extend(find_mm_symbols(child))
-
-    return symbols
 
 def try_replace(text, symbols):
     for sym in symbols:
@@ -88,8 +64,10 @@ def try_replace(text, symbols):
             elif index + length == len(text): # match at the end
                 text = '=pv(concat("%s", %s))' % (text[:-length], sym_loc_ts)
             else:  # match in the middle
-                text = '=pv(concat("%s", %s, "%s"))' % (text[:index], sym_loc_ts, text[index+length:])
+                text = '=pv(concat("%s", %s, "%s"))' %\
+                         (text[:index], sym_loc_ts, text[index+length:])
         return text
+
 
 def replace_symbols(node, symbols):
     '''
@@ -103,20 +81,20 @@ def replace_symbols(node, symbols):
         for child in node:
             replace_symbols(child, symbols)
 
-def parse(path):
-    tree = et.parse(path)
+def parse(filepath):
+    tree = et.parse(filepath)
     root = tree.getroot()
 
     mm_symbols = find_mm_symbols(root)
-    print "There are %s mm_symbols." % len(mm_symbols)
+    print 'There are %s mm_symbols:' % len(mm_symbols)
     print mm_symbols
 
     replace_symbols(root, mm_symbols)
 
     # write the new tree out to the same file
-    make_writeable(file)
-    tree.write(file, encoding='utf-8', xml_declaration=True)
-    make_read_only(file)
+    make_writeable(filepath)
+    tree.write(filepath, encoding='utf-8', xml_declaration=True)
+    make_read_only(filepath)
 
 
 if __name__ == '__main__':
@@ -124,7 +102,7 @@ if __name__ == '__main__':
     try:
         path_file = sys.argv[1]
     except IndexError:
-        print "Usage: ", sys.argv[0], "<path-file>"
+        print 'Usage: ', sys.argv[0], '<path-file>'
         sys.exit()
 
     with open(path_file) as f:
@@ -133,8 +111,8 @@ if __name__ == '__main__':
         lines = [line.strip() for line in lines if not line == '']
 
 
-    for file in lines:
-        print "Parsing file ", file
-        parse(file)
+    for filepath in lines:
+        print 'Parsing file', filepath
+        parse(filepath)
 
 
