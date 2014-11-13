4e4272d0cd2f59da1cfb1a56c332ec4e413af73d
Will Rogers <wahrogers@gmail.com>
2014-11-13 19:00:59 +0000

    Remove redundant execution code from package.

--- convert/converter.py
+++ convert/converter.py
@@ -29,13 +29,8 @@ import glob
 import subprocess
 import shutil
 import string
-import ConfigParser
-import argparse
 
 import logging as log
-LOG_FORMAT = '%(levelname)s:  %(message)s'
-LOG_LEVEL = log.INFO
-log.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL)
 
 
 NULL_FILE = open(os.devnull, 'w')
@@ -292,99 +287,6 @@ def store_symbol(source, destination, symbol_dictionary):
         symbol_dictionary[source] = [destination]
 
 
-def set_up_options():
-    parser = argparse.ArgumentParser(description='''
-    Convert whole areas of EDM screens into CSS's OPI format.
-    Configuration files for each area are found in the conf/
-    directory.  The output location is not automatically
-    created to avoid unwanted files...
-    ''')
-    parser.add_argument('-f', action='store_true', dest='force',
-                        help='overwrite existing OPI files')
-    parser.add_argument('config', metavar='<config-file>', nargs='*',
-                        help='config file specifying EDM paths and output dir')
-    args = parser.parse_args()
-    return args
-
-
-def parse_config(cfg):
-    """ Parse a specified configuration file
-
-        Raises ConfigurationError if critical section is missing
-
-        Returns:
-            script_file,
-            script_args,
-            symbols,
-            outdir
-    """
-
-    log.info('\n\nStarting config file %s.\n', cfg)
-    cp = ConfigParser.ConfigParser()
-    cp.read(cfg)
-
-    try:
-        script_file = cp.get('edm', 'edm_script')
-    except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-        raise ConfigurationError()
-
-    try:
-        script_args = cp.get('edm', 'script_args')
-        script_args = script_args.split()
-    except ConfigParser.NoOptionError:
-        script_args = []
-
-    try:
-        outdir = cp.get('opi', 'outdir')
-    except ConfigParser.NoSectionError:
-        raise ConfigurationError()
-
-    try:
-        symbols = cp.get('edm', 'symbols')
-        symbols = symbols.split(':')
-    except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-        symbols = []
-
-    return script_file, script_args, symbols, outdir
-
-
-def run_conversion():
-    """ Perform the module conversion.
-        This is the entry point for the module
-    """
-
-    symbol_dict = {}
-
-    # Parse configuration
-    args = set_up_options()
-    log.debug('Config files supplied: %s', args.config)
-
-    try:
-        if not os.path.isdir(TMP_DIR):
-            os.makedirs(TMP_DIR)
-        if not os.path.isdir(SYMBOLS_DIR):
-            os.makedirs(SYMBOLS_DIR)
-    except OSError:
-        log.error('Could not create temporary directories %s and %s',
-                  (TMP_DIR, SYMBOLS_DIR))
-
-    for cfg in args.config:
-        try:
-            (script_file, script_args, symbols, outdir) = parse_config(cfg)
-
-            for sym in symbols:
-                print "Found symbol: " + sym
-
-            c = Converter(script_file, script_args, symbols, outdir, symbol_dict)
-            c.convert(args.force)
-        except ConfigurationError:
-            log.error('Please ensure %s is a valid config file' % args.config)
-
-    log.info("Post-processing symbol files")
-    for path, destinations in symbol_dict.iteritems():
-        convert_symbol(path, destinations)
-
-
 def convert_symbol(symbol_file, destinations):
     """
     Convert an EDM symbol file into the png used by the CSS symbol widget.
@@ -459,5 +361,3 @@ def convert_edl(filename, destination):
             log.info("Conversion return code: %s" % returncode)
             utils.make_read_only(destination)
 
-if __name__ == '__main__':
-    run_conversion()
