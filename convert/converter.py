3ec0393916a56a8fb1953504b199b8f3f5827a1c
Will Rogers <wahrogers@gmail.com>
2014-11-14 17:20:57 +0000

    Move functions out of converter.py into files.py.
    Update tests.

--- convert/converter.py
+++ convert/converter.py
@@ -24,28 +24,20 @@ Steps:
 import utils
 import paths
 import compress
+import files
 
 import os
 import glob
-import subprocess
 import shutil
 import string
 
 import logging as log
 
 
-NULL_FILE = open(os.devnull, 'w')
-TMP_DIR = './tmp'
-SYMBOLS_DIR = './tmp/symbols'
 
 OPI_EXT = 'opi'
 EDL_EXT = 'edl'
 
-# Commands in lists for subprocess
-CONVERT_CMD = ['java', '-Dedm2xml.colorsFile=res/colors.list', '-jar', 'res/conv.jar']
-UPDATE_CMD = ['edm', '-convert']
-SYMB_SCRIPT = os.path.join(os.getcwd(), 'res/auto-symb.sh')
-COMPRESS_CMD = [SYMB_SCRIPT]
 
 PROJECT_TEMPLATE = 'res/project.template'
 PROJECT_FILENAME = '.project'
@@ -212,7 +204,7 @@ class Converter(object):
                 store_symbol(full_path, destination, self.symbol_files)
                 log.info('Successfully stored symbol file %s', destination)
             else:
-                convert_edl(full_path, destination)
+                files.convert_edl(full_path, destination)
                 log.info('Successfully converted %s', destination)
                 self.update_paths(destination, depth)
         except Exception as e:
@@ -286,92 +278,4 @@ def store_symbol(source, destination, symbol_dictionary):
             destinations.append(destination)
     else:
         symbol_dictionary[source] = [destination]
-
-
-def convert_symbol(symbol_file, destinations):
-    """
-    Convert an EDM symbol file into the png used by the CSS symbol widget.
-    This uses an external shell script.
-    """
-    log.debug("Converting symbol %s", symbol_file)
-    dest = os.path.join(SYMBOLS_DIR, os.path.basename(symbol_file))
-    utils.make_writeable(dest)
-    shutil.copyfile(symbol_file, dest)
-    # Update EDM file if necessary.
-    if is_old_edl(dest):
-        dest = update_edl(dest)
-    # Compress EDM symbol file to minimum rectangle.
-    compress.parse(dest)
-    command = COMPRESS_CMD + [symbol_file]
-    out = subprocess.check_output(" ".join(command), shell=True)
-    # copy png to right location
-    relfilename = out.strip()
-    filename = os.path.basename(relfilename)
-    source = os.path.join(os.getcwd(), relfilename)
-
-    # Copy the converted png to all specified destinations
-    for destination in destinations:
-        log.debug("... copy to %s", destination)
-        try:
-            utils.make_writeable(destination)
-            destdir = os.path.dirname(destination)
-            absfilename = os.path.join(destdir, filename)
-
-            utils.make_writeable(absfilename)
-            shutil.copyfile(source, absfilename)
-            utils.make_read_only(absfilename)
-        except Exception as e:
-            log.error("Failed copying file: %s", str(e))
-
-
-def is_old_edl(filename):
-    """
-    Check version of .edl file.  Versions < 3 need updating.
-    """
-    with open(filename) as edm_file:
-        for line in edm_file.readlines():
-            if line.isspace() or line.strip().startswith('#'):
-                continue
-            return int(line.split()[0]) < 4
-
-def update_edl(filename):
-    """
-    Copy EDM file to temporary location.  Attempt to convert to
-    new format using EDM. Return location of converted file.
-    """
-    print 'updating', filename
-    try:
-        tmp_edm = os.path.join(TMP_DIR, os.path.basename(filename))
-        print 'dest is', tmp_edm
-        # make sure we have write permissions on the destination
-        utils.make_writeable(tmp_edm)
-        shutil.copyfile(filename, tmp_edm)
-        cmd = UPDATE_CMD + [tmp_edm]
-        print 'updated %s with %s' % (tmp_edm, cmd)
-        returncode = subprocess.call(cmd, stdout=NULL_FILE, stderr=NULL_FILE)
-        if returncode == 0:
-            utils.make_writeable(tmp_edm)
-            return tmp_edm
-        else:
-            log.warn('EDM update failed with code %s', returncode)
-    except Exception as e:
-        log.error("Failed copying file" + str(e))
-
-    return None  # else and Exception cases
-
-
-def convert_edl(filename, destination):
-    """
-    Try to convert .edl file.  If it fails, try updating .edl file
-    using edm before converting again.
-    """
-    if is_old_edl(filename):
-        filename = update_edl(filename)
-    utils.make_writeable(destination)
-    log.debug("Converting %s", filename)
-    command = CONVERT_CMD + [filename, destination]
-    returncode = subprocess.call(command)
-    utils.make_read_only(destination)
-    if returncode != 0:  # conversion failed
-        log.warn('Conversion failed with code %d.', returncode)
 
--- test/test_convert.py
+++ test/test_convert.py
@@ -1,5 +1,5 @@
 
-from convert.converter import store_symbol, is_old_edl
+from convert.converter import store_symbol
 import unittest
 
 
@@ -30,15 +30,6 @@ class SymbolDictionaryTest(unittest.TestCase):
 
         self.assertDictEqual({"path": ["dest", "new_dest"]}, symbol_dict)
 
-class OldEdlTest(unittest.TestCase):
-
-    def test_is_old_edl_recognises_old(self):
-        old_edl_file = '/dls_sw/prod/R3.14.11/support/vxStats/1-14/data/FE-IOCs.edl'
-        self.assertTrue(is_old_edl(old_edl_file))
-
-    def test_is_old_edl_recognises_new(self):
-        new_edl_file = '/dls_sw/prod/R3.14.11/support/vxStats/1-14/data/diamond.edl'
-        self.assertFalse(is_old_edl(new_edl_file))
 
 if __name__ == '__main__':
     unittest.main()
