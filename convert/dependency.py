1bfc2b7262b47b76c3b763b356b9d5471915473b
Nick Battam <nicholas.battam@diamond.ac.uk>
2015-08-11 15:13:53 +0100

    Update dependency finder API to take explicit path and area

--- convert/dependency.py
+++ convert/dependency.py
@@ -1,44 +1,50 @@
-from pkg_resources import require
-
-require("dls_environment")
-
 import dls_epicsparser.releaseparser
 import os
 
-from convert.utils import increment_version
-
-
-SVN_ROOT = 'svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls'
-TRUNK = 'diamond/trunk'
-IOC_DIR = '/dls_sw/prod/R3.14.12.3/ioc/'
 CONFIGURE_RELEASE = 'configure/RELEASE'
-MIRROR_ROOT = '/dls_sw/work/common/CSS/converter/output/mirror_root'
+EPICS_BASE = '/dls_sw/epics/R3.14.12.3/base'
 
 
 class DependencyParser(object):
 
-    def __init__(self, ioc_name, version):
+    def __init__(self, root, area, module_name, version):
         """ Parse dependencies for IOC
 
-        :param ioc_name: IOC name (e.g. "LI/TI")
+        :param root: Base path to search ('/dls_sw/prod/R3.14.12.3')
+        :param area: Module area: 'support' or 'ioc'
+        :param module_name: IOC/support module name (e.g. "LI/TI")
         :param version: Version number to inspect (e.g. "5-4")
         """
-        self._ioc_name = ioc_name
-        self._ioc_version = version
-        self.dependencies = {}
-
-        self.find_dependencies()
+        self._module_path = os.path.join(root, area, module_name, version)
 
     def find_dependencies(self):
         """ Generate a dictionary of dependencies for this module
-            {dependency name => path to version+1}
+            {dependency name => path to version++1}
         """
-        cr_path = os.path.join(
-            IOC_DIR, self._ioc_name, self._ioc_version, CONFIGURE_RELEASE)
+        dependencies = {}
 
+        cr_path = os.path.join(self._module_path, CONFIGURE_RELEASE)
         r = dls_epicsparser.releaseparser.Release(cr_path)
 
-        for p in r.flatten():
-            if p.path is not None:
-                print p.path
-                self.dependencies[p.name] = os.path.join('/', increment_version(p.path[1:]))
+        print r
+        for dependency in r.flatten():
+            if self.is_valid(dependency):
+                dependencies[dependency.name] = (os.path.split(dependency.path))
+
+        return dependencies
+
+    def is_valid(self, dependency):
+        """ Simple test that dependency is 'valid'.
+            i.e path and name defined,
+                not EPICS base, or a child
+                not the searched module
+
+        :param dependency: Dependency to check with .path and .name attributes
+        :return: True if valid dependency
+        """
+        valid = dependency.path is not None
+        valid = valid and dependency.name is not None
+        valid = valid and not dependency.path.startswith(EPICS_BASE)
+        valid = valid and not dependency.path == self._module_path
+
+        return valid
