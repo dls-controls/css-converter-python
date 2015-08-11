4da37e9b7fe455e077aac006acd74dac7bbad9b0
Will Rogers <wahrogers@gmail.com>
2015-08-11 11:00:36 +0100

    More detailed argument parsing.

--- convert_module.py
+++ convert_module.py
@@ -1,6 +1,13 @@
-
-
+import os
+import sys
 import argparse
+import ConfigParser
+
+
+IOC_CONFIG = 'ioc.ini'
+SUPPORT_CONFIG = 'support.ini'
+GENERAL_CONFIG = 'converter.ini'
+
 
 def parse_arguments():
     ap = argparse.ArgumentParser()
@@ -11,7 +18,33 @@ def parse_arguments():
     ap.add_argument('-c', help='configuration file directory',
                     metavar='<config-file>',
                     default='conf/')
-    return ap.parse_args()
+    args = ap.parse_args()
+    if args.s == args.i:
+        print('Please choose either IOC [-i] or support module [-s].')
+        sys.exit()
+    if args.s:
+        args.support_config = os.path.join(args.c, SUPPORT_CONFIG)
+    else:
+        args.ioc_config = os.path.join(args.c, IOC_CONFIG)
+    args.general_config = os.path.join(args.c, GENERAL_CONFIG)
+    if not os.path.exists(args.general_config):
+        print('Could not locate configuration file {}'.format(args.general_config))
+        sys.exit()
+
+    return args
+
+
+def parse_configuration(filepath):
+    config = ConfigParser.ConfigParser()
+    config.read(filepath)
+    return config
+
 
 if __name__ == '__main__':
     args = parse_arguments()
+    gen_cfg = parse_configuration(args.general_config)
+    if args.i:
+        ioc_cfg = parse_configuration(args.ioc_config)
+    else:
+        support_cfg = parse_configuration(args.support_config)
+    print(gen_cfg.get('general', 'java'))
