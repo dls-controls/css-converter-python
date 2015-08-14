824bfbaf2c8a59c28257bc30a99ad25959cd6392
Nick Battam <nicholas.battam@diamond.ac.uk>
2015-08-14 14:33:14 +0100

    Remove unrequired config file lookup

--- convert_module.py
+++ convert_module.py
@@ -150,13 +150,11 @@ if __name__ == '__main__':
         dependencies = mod.get_dependencies()
         edl_dirs = [mod.get_edl_path()]
         for dep, dep_coords in dependencies.items():
-            mod_cfg = get_config_section(cfg, mod.coords.module)
-
             new_version = utils.increment_version(dep_coords.version)
             dep_edl_path = os.path.join(mirror,
                                       coordinates.as_path(dep_coords, False)[1:],
                                       new_version,
-                                      mod_cfg['datapath'])
+                                      mod.edl_dir)
             edl_dirs.append(dep_edl_path)
 
         file_dict = paths.index_paths(edl_dirs, True)
