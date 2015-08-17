7fb1aae5ba7f18d59b0530e030ad87fb974f3273
Nick Battam <nicholas.battam@diamond.ac.uk>
2015-08-17 17:21:36 +0100

    Build css launch script from configure/RELEASE dependency tree

---
+++ build_runcss.py
@@ -0,0 +1,130 @@
+# from pkg_resources import require
+# require("dls_css_converter")
+
+""" This script populates a runcss.template with
+
+- location of released CSS executable
+- list of all module dependencies as { full-path-to-module-version=module-version/moduleApp }
+- take launch opi as an argument 'xxx.opi' or 'yyy/xxx.opi'
+"""
+import ConfigParser
+import os
+import logging as log
+import string
+import stat
+
+from convert import dependency, coordinates
+
+SCRIPT_FILE = 'runcss.sh'
+SCRIPT_TEMPLATE = 'res/runcss.template'
+ESCAPE_CHARS = ['.', ':']
+PATH_PREFIX = '${prefix}'
+
+VERSION_FILE = 'configure/VERSION'
+MODULE_INI = 'configure/module.ini'
+
+
+def get_version(coord):
+    """ Read the 'new' version number for the VERSION file
+
+        :return Version string (e.g. '4-2dls2')
+    """
+    version = ""
+    path = os.path.join(coordinates.to_path(coord), VERSION_FILE)
+    if os.path.exists(path):
+        with open(path, 'r') as content_file:
+            version = content_file.read()
+
+    return version.rstrip()
+
+
+def parse_opi_path(coord):
+    """ Extract the opi-file location in the module from the module.ini file
+
+    :raises:
+    :param coord: Module coordinate (inc. version)
+    :return: Relative path to OPI files, e.g. MyModuleApp/opi/opi
+    """
+    path = coordinates.as_path(coord)
+    print "Reading opiPath from %s" % path
+
+    if coord.version is None:
+        raise ValueError("Dependency with unspecified version: %s", path)
+
+    # plausible default...
+    opi_path = "%sApp/opi/opi" % coord.module
+    module_ini_path = os.path.join(path, MODULE_INI)
+
+    if os.path.exists(module_ini_path):
+        try:
+            parser = ConfigParser.ConfigParser()
+            parser.read(module_ini_path)
+
+            opi_path = parser.get('files', 'opi')
+        except ConfigParser.NoSectionError:
+            # file doesn't exist so...
+            print "No [files] in module.ini file in %s using default %s" % (path, opi_path)
+    else:
+        print "No module.ini file in %s using default %s" % (path, opi_path)
+
+    return opi_path
+
+
+def build_links(dependencies, project_name):
+
+    links_strings = []
+    for dep, dep_coord in dependencies.iteritems():
+        opi_path = parse_opi_path(dep_coord)
+        links_strings.append('%s%s=%s' % (PATH_PREFIX,
+                                          os.path.join(coordinates.as_path(dep_coord), opi_path),
+                                          os.path.join('/', project_name, dep)))
+
+    links = ',\\\n'.join(links_strings)
+
+    for c in ESCAPE_CHARS:
+        links = links.replace(c, '[\%d]' % ord(c))
+
+    return links
+
+
+def gen_run_script(coord):
+    '''
+    Generate a wrapper script which updates the appropriate
+    links before opening a CSS window.
+
+    :param coord: coordindate of module being released, including the release
+                    version number. Root should specify the *build* location
+    '''
+
+    opi_dir = parse_opi_path(coord)
+    script_dir = os.path.join(coordinates.as_path(coord), opi_dir)
+    if not os.path.exists(script_dir):
+        os.makedirs(script_dir)
+    script_path = os.path.join(script_dir, SCRIPT_FILE)
+
+    dependencies = dependency.DependencyParser(coord)
+
+
+    project_name = "%s_%s" % (coord.module.replace(os.path.sep, '_'), coord.version)
+    links_string = build_links(dependencies.find_dependencies(), project_name)
+
+    with open(script_path, 'w') as f:
+        with open(SCRIPT_TEMPLATE) as template:
+            content = template.read()
+            s = string.Template(content)
+            updated_content = s.substitute(links=links_string)
+            f.write(updated_content)
+
+    # Give owner and group execute permissions.
+    st = os.stat(script_path)
+    os.chmod(script_path, st.st_mode | stat.S_IXUSR | stat.S_IXGRP)
+    log.info('Run script written to %s', script_path)
+    return script_path
+
+
+if __name__ == '__main__':
+    # Call with:
+    #   root, area, module arguments.
+    version = get_version(coordinates.create(root, area, module))
+    module_coord = coordinates.create(root, area, module, version)
+    gen_run_script(module_coord)
