7446cf7d4d60c1467581fd60e4b482529f8896cb
Will Rogers <wahrogers@gmail.com>
2015-08-18 14:35:54 +0100

    Improve tests for module.py.

--- test/test_module.py
+++ test/test_module.py
@@ -2,6 +2,7 @@ import pkg_resources
 pkg_resources.require('dls_epicsparser')
 import os
 import unittest
+import mock
 from convert import module
 from convert import coordinates
 
@@ -9,13 +10,24 @@ from convert import coordinates
 class ModuleTest(unittest.TestCase):
 
     def setUp(self):
-        self.coords = coordinates.from_path2('/dls_sw/prod/R3.14.12.3/ioc/LI/TI/5-3')
-        print('The coordinates are {}'.format(self.coords))
+        self.version = '5-3'
+        self.new_version = '5-4'
+        self.module_path = '/dls_sw/prod/R3.14.12.3/ioc/LI/TI/'
+        self.edl_path = 'MyApp/opi/opi'
+        self.coords = coordinates.from_path2(os.path.join(self.module_path,
+                                                          self.version))
         self.mirror_root = '/tmp/mirror'
-        dummy_cfg = {'edl_dir': '.',
+        self.dummy_cfg = {'edl_dir': self.edl_path,
                      'opi_dir': '.',
                      'extra_deps': []}
-        self.m = module.Module(self.coords, dummy_cfg, self.mirror_root)
+        # Avoid checking that the directory exists
+        with mock.patch('os.path.exists') as mp:
+            mp.return_value = True
+            self.m = module.Module(self.coords, self.dummy_cfg, self.mirror_root)
+
+    def test_constructor_throws_ValueError_if_module_does_not_exist(self):
+        dud_path = coordinates.from_path2('/dls_sw/prod/R3.14.12.3/ioc/dummy/1-1')
+        self.assertRaises(ValueError, module.Module, dud_path, self.dummy_cfg, self.mirror_root)
 
     def test_get_dependencies(self):
         """
@@ -27,11 +39,10 @@ class ModuleTest(unittest.TestCase):
         self.assertNotIn(self.m.coords, deps.values())
 
     def test_get_edl_path(self):
-        """
-        This is just turning the method inside-out.
-        """
-        edl_path = os.path.join(self.m.mirror_root, self.m.new_module_dir[1:],
-                                self.m.edl_dir)
+        edl_path = os.path.join(self.mirror_root,
+                                self.module_path[1:],
+                                self.new_version,
+                                self.edl_path)
         self.assertEqual(os.path.normpath(edl_path), self.m.get_edl_path())
 
     def test_convert(self):
