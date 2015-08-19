8aeec753fc6c31bad0b108c4f39d6399a48e4a2d
Will Rogers <wahrogers@gmail.com>
2015-08-19 09:22:19 +0100

    Update spoof.py tests by updating current versions.
    This shows that they are not good unit tests.

--- test/test_spoof.py
+++ test/test_spoof.py
@@ -4,6 +4,10 @@ from convert.spoof import _from_configure_ioc, _is_edm_script
 import unittest
 
 class TestSpoof(unittest.TestCase):
+    """
+    A lot of these tests are fragile because they depend on the filesystem
+    setup.
+    """
 
     def test_configure_ioc(self):
         """
@@ -17,12 +21,11 @@ class TestSpoof(unittest.TestCase):
         This might fail when the actual output changes!
         """
         out = _from_configure_ioc('CS-DI-IOC-09', 'sofb-gui')
-        self.assertEqual(out, '/dls_sw/prod/R3.14.12.3/ioc/CS/CS-DI-IOC-09/1-46/sofb-gui')
+        self.assertEqual(out, '/dls_sw/prod/R3.14.12.3/ioc/CS/CS-DI-IOC-09/1-54/sofb-gui')
 
     def test_is_edm_script_fofb(self):
-        out = _is_edm_script('/dls_sw/prod/R3.14.12.3/support/fastfeedback/11-11/fofbApp/opi/fofb-gui')
+        out = _is_edm_script('/dls_sw/prod/R3.14.12.3/support/fastfeedback/11-21/fofbApp/opi/fofb-gui')
         self.assertTrue(out)
 
-    def test_is_edm_script_fofb(self):
-        out = _is_edm_script('/dls_sw/prod/R3.14.12.3/support/fastfeedback/11-11/fofbApp/opi/fofb-gui')
-        self.assertTrue(out)
+if __name__ == '__main__':
+    unittest.main()
