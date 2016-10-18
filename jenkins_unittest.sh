9e5823e5b42a3daf3eaf50c028206ec85c2c43c7
Nick Battam <nicholas.battam@diamond.ac.uk>
2016-10-18 15:59:31 +0100

    Update handling of failures

--- jenkins_unittest.sh
+++ jenkins_unittest.sh
@@ -5,16 +5,16 @@ module load pylint
 module list
 export PYTHONPATH=$PYTHONPATH:${WORKSPACE}/
 
+# Explicitly set the shell to continue if an step fails
+# this is required because pylint has a non-zero return code if there 
+# are any issues (see pylint --long-help)
+set +e
+
 # create a working dir for the logfile/images
 mkdir logs
 
 # run tests
 nosetests --with-xunit --xunit-file=${WORKSPACE}/logs/nosetests.xml
-
-# Explicitly set the shell to continue if an step fails
-# this is required because pylint has a non-zero return code if there 
-# are any issues (see pylint --long-help)
-set +e
 
 # run pylint
 touch ${WORKSPACE}/__init__.py
