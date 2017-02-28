2f6b6430b95271db05735e1de188df81b606b1e1
Will Rogers <wahrogers@gmail.com>
2017-02-28 17:07:06 +0000

    Tests for version comparing.

--- test/test_html_versions.py
+++ test/test_html_versions.py
@@ -3,18 +3,36 @@ import pkg_resources
 pkg_resources.require('jinja2')
 pkg_resources.require('pytest')
 
-import html_versions
+from html_versions import ModDetails, render, RESOURCES_DIR, JSON_TEMPLATE
 import jinja2
 import os
 import json
+import pytest
 
 
-def test_render():
-    test_mod_details = html_versions.ModDetails('dummy')
-    test_mod_details.deps['dummy2'] = html_versions.ModDetails('dummy1')
-    res_dir = os.path.join(os.curdir, html_versions.RESOURCES_DIR)
+@pytest.fixture
+def dummy_md():
+    return ModDetails('dummy')
+
+
+def test_render_creates_valid_json(dummy_md):
+    dummy_md.deps['dummy2'] = ModDetails('dummy1')
+    res_dir = os.path.join(os.curdir, RESOURCES_DIR)
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(res_dir))
-    ans = html_versions.render([test_mod_details], env, html_versions.JSON_TEMPLATE)
-    print(ans)
+    ans = render([dummy_md], env, JSON_TEMPLATE)
     json.loads(ans)
-    assert False
+
+
+@pytest.mark.parametrize('cfg,latest,expected', [
+        ('1-0', '1-1', ModDetails.OUT_OF_DATE),
+        ('1-0', '1-0', ModDetails.OK),
+        ('1-1', '1-0', ModDetails.OK),
+        ('1-0', '1-0-1', ModDetails.OUT_OF_DATE),
+        ('2-8dls4-8', '2-8dls4-8-1', ModDetails.OUT_OF_DATE)
+    ])
+def test_assess_cfg_ioc_versions_handles_different_cases(dummy_md,
+                                                         cfg,
+                                                         latest,
+                                                         expected):
+    md = ModDetails('dummy', cfg_ioc_version=cfg, latest_release=latest)
+    assert md.cfg_ioc_version_class == expected
